b197a7c1f79435be7278694ed88419feb7d7c044
Tihomir Penev <tish.peneff@gmail.com>
2017-01-06 10:33:42 -0500

    Add files via upload

---
+++ Recursion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+
+
+public  class Recursion
+{
+       static void Main(string[] args)
+       {
+        TraverseDirectory("d:/ss");
+       }
+
+    public static void TraverseDirectory(string directory)
+    {
+        
+        Console.WriteLine($"--{directory}--");
+        
+
+        string[] allFilles = Directory.GetFiles(directory);
+
+        foreach (var file in allFilles)
+        {
+            Console.WriteLine(file);
+        }
+
+        string[] allDirs = Directory.GetDirectories(directory);
+
+        foreach (var dir in allDirs)
+        {
+            TraverseDirectory(dir);
+        }
+    }
+}
+
